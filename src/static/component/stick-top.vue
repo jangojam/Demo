90e5d91e94c9bc8a1c77ef08dda4537890c41641
jango <821532623@qq.com>
2018-03-23 16:23:48 +0800

    'stick-to-top-demo'

---
+++ src/static/component/stick-top.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <div ref="A" class="A">
+      <p>东风浩荡东风浩荡东风浩荡</p> 
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p> 
+    </div>
+    <div class="B"  :class="stick ? 'topFix': ''"></div>
+    <div class="B" v-show="stick"></div>
+    <div class="C">
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>  
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>   
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p> 
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p>
+      <p>东风浩荡东风浩荡东风浩荡</p> 
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        stick: false,
+        
+      }
+    },
+    mounted() {
+      
+    },
+    created() {
+      this.$nextTick(function(){
+        window.addEventListener('scroll',this.scrollFb)
+      })
+    },
+    computed: {
+    },
+    methods: {
+      scrollFb: function() {
+        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+        var AHeight = this.$refs.A.offsetHeight
+        console.log(scrollTop,AHeight)
+        if(scrollTop >= AHeight){
+          this.stick = true;
+        } else {
+          this.stick = false;
+        }
+      }
+    }
+
+  }
+</script>
+
+
+ <style lang="scss" src="../css/stick-top.scss" scoped>
+ </style>
---
+++ src/static/js/stick-top.js
@@ -0,0 +1,5 @@
+import stickTop from '../component/stick-top.vue'
+import Vue from 'vue'
+new Vue({
+  render: h => h(stickTop)
+}).$mount('#app')
